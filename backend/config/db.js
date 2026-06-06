72f6c33926489ecca4d60532968a2cd08717d764
mosemwita <mosemwita496@gmail.com>
2026-06-06 15:43:21 +0300

    fix db connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,17 +1,20 @@
-
 import mysql from 'mysql2/promise';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 const pool = mysql.createPool({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
+  host:     process.env.DB_HOST,
+  user:     process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port:     Number(process.env.DB_PORT) || 3306,
+  ssl: {
+    rejectUnauthorized: true
+  },
   waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
+  connectionLimit:    10,
+  queueLimit:         0,
 });
 
 export default pool;
